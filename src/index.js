19edc17a0c203ebdeb7ae3c2d0041e938fd15a5d
Vincent Garrigues <vincent.garrigues@gmail.com>
2016-02-07 16:46:24 +0100

    It's running

---
+++ src/index.js
@@ -0,0 +1,58 @@
+/* @flow */
+
+import express from 'express';
+
+import {
+  GraphQLID,
+  GraphQLString,
+  GraphQLObjectType,
+  GraphQLSchema
+} from 'graphql';
+
+var graphqlHTTP = require('express-graphql');
+
+var app = express();
+
+var trackType = new GraphQLObjectType({
+  name: 'Track',
+  description: 'A track on SoundCloud.',
+  fields: () => ({
+    id: {
+      type: GraphQLID,
+      description: 'The identifier of the Track.',
+      resolve: (track) => track.id
+    },
+    title: {
+      type: GraphQLString,
+      description: 'The title of the track.',
+      resolve: (track) => track.title
+    }
+  })
+});
+
+var rootType = new GraphQLObjectType({
+  name: 'Root',
+  fields: () => ({
+    track: {
+      type: trackType,
+      args: {
+        id: { type: GraphQLID }
+      },
+      description: 'Find track by id',
+      resolve: (_, args) => {
+        if (args.id !== undefined && args.id !== null) {
+          return { id: args.id, title: 'Title ' + args.id };
+        }
+        throw new Error('must provide id');
+      }
+    }
+  })
+});
+
+var schema = new GraphQLSchema({ query: rootType });
+
+app.use('/graphql', graphqlHTTP({ schema: schema, graphiql: true }));
+
+app.listen(3000, function () {
+  console.log('Listening on port 3000!');
+});
