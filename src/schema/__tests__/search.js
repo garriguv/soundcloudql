f0a163b3f151dabd462a755d2499f60773f95765
Vincent Garrigues <vincent.garrigues@gmail.com>
2016-02-27 14:49:45 +0100

    Clear cache

--- src/schema/__tests__/search.js
+++ src/schema/__tests__/search.js
@@ -6,23 +6,23 @@ import { soundcloud } from './soundcloudql';
 
 describe('Search type', function () {
   it('Searches for tracks', function () {
-    var query = `{ searchTracks(q: "Boiler Room", limit: 2) { collection { title } }}`;
+    var query = `{ searchTracks(q: "Boiler Room", limit: 1) { collection { title } }}`;
     return soundcloud(query).then(function (result) {
       expect(result.data.searchTracks.collection[0].title).to.equal('Carl Cox 45 min Boiler Room Ibiza Villa Takeovers mix');
     });
   });
 
   it('Searches for users', function () {
-    var query = `{ searchUsers(q: "Eric", limit: 2) { collection { username } }}`;
+    var query = `{ searchUsers(q: "Eric", limit: 1) { collection { username } }}`;
     return soundcloud(query).then(function (result) {
       expect(result.data.searchUsers.collection[0].username).to.equal('Eric Prydz');
     });
   });
 
   it('Searches for playlists', function () {
-    var query = `{ searchPlaylists(q: "Robot Heart", limit: 2) { collection { title } }}`;
+    var query = `{ searchPlaylists(q: "Eric", limit: 1) { collection { title } }}`;
     return soundcloud(query).then(function (result) {
-      expect(result.data.searchPlaylists.collection[0].title).to.equal('Robot Heart');
+      expect(result.data.searchPlaylists.collection[0].title).to.equal('Eric Prydz presents EPIC Radio');
     });
   });
 });
